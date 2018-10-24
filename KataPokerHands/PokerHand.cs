1c52d2dd73e8fe2eb950111c528e59c1ffe6ce78
Romain BERTHON <Romain BERTHON>
2018-10-24 09:58:35 +0200

    Get winnig hand between two hands with different weights

---
+++ KataPokerHands/PokerHand.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Value;
+
+namespace KataPokerHands
+{
+    public class PokerHand : ValueType<PokerHand>
+    {
+        private readonly Card card1;
+        private readonly Card card2;
+
+        public PokerHand(Card card1, Card card2)
+        {
+            this.card1 = card1;
+            this.card2 = card2;
+        }
+
+        private Card GetHighestCard() => card1 > card2 ? card1 : card2;
+        private Card GetLowestCard() => card1 < card2 ? card1 : card2;
+
+        public static PokerHand GetBestHand(PokerHand hand1, PokerHand hand2)
+        {
+            if (hand1.GetHighestCard() > hand2.GetHighestCard())
+                return hand1;
+            else if (hand1.GetHighestCard() < hand2.GetHighestCard())
+                return hand2;
+            else if (hand1.GetLowestCard() > hand2.GetLowestCard())
+                return hand1;
+            return hand2;
+        }
+
+        public override string ToString() => $"{nameof(PokerHand)} [{card1}; {card2}]";
+
+        protected override IEnumerable<object> GetAllAttributesToBeUsedForEquality()
+        {
+            return new object[] { card1, card2 };
+        }
+    }
+
+    public class Card : ValueType<Card>
+    {
+        private readonly Suits suit;
+        private readonly Values value;
+
+        public Card(Suits suit, Values value)
+        {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        protected override IEnumerable<object> GetAllAttributesToBeUsedForEquality()
+        {
+            return new object[] { suit, value };
+        }
+
+        public override string ToString() => $"{nameof(Card)} [{suit}-{value}]";
+
+        public static bool operator >(Card left, Card right) => left.value > right.value;
+        public static bool operator <(Card left, Card right) => left.value < right.value;
+    }
+
+    public enum Suits
+    {
+        Clubs, Diamonds, Hearts, Spades
+    }
+
+    public enum Values
+    {
+        Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+    }
+}
---
+++ KataPokerHands/PokerHandShould.cs
@@ -0,0 +1,37 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace KataPokerHands
+{
+    [TestFixture]
+    public class PokerHandShould
+    {
+        private static readonly Card SmallCard = new Card(Suits.Hearts, Values.Two);
+        private static readonly Card BigCard = new Card(Suits.Hearts, Values.Ace);
+
+        private static readonly (PokerHand, PokerHand)[] FirstHandWins =
+        {
+            (new PokerHand(new Card(Suits.Clubs, Values.King), SmallCard), new PokerHand(new Card(Suits.Spades, Values.Queen), SmallCard)),
+            (new PokerHand(new Card(Suits.Clubs, Values.Ace), SmallCard), new PokerHand(SmallCard, new Card(Suits.Spades, Values.Ten))),
+            (new PokerHand(SmallCard, new Card(Suits.Clubs, Values.Ace)), new PokerHand(new Card(Suits.Spades, Values.Ten), SmallCard)),
+            (new PokerHand(SmallCard, new Card(Suits.Clubs, Values.Ace)), new PokerHand(SmallCard, new Card(Suits.Spades, Values.Ten))),
+            (new PokerHand(new Card(Suits.Clubs, Values.King), BigCard), new PokerHand(new Card(Suits.Spades, Values.Queen), BigCard)),
+            (new PokerHand(BigCard, new Card(Suits.Clubs, Values.King)), new PokerHand(new Card(Suits.Spades, Values.Queen), BigCard)),
+            (new PokerHand(new Card(Suits.Clubs, Values.King), BigCard), new PokerHand(BigCard, new Card(Suits.Spades, Values.Queen))),
+            (new PokerHand(BigCard, new Card(Suits.Clubs, Values.King)), new PokerHand(BigCard, new Card(Suits.Spades, Values.Queen))),
+        };
+
+        [Test]
+        [TestCaseSource(nameof(FirstHandWins))]
+        public void ReturnFirstHandWhenTheBestOfTwoHands((PokerHand, PokerHand) @case)
+        {
+            (PokerHand winningHand, PokerHand losingHand) = @case;
+
+            var result1 = PokerHand.GetBestHand(winningHand, losingHand);
+            Check.That(result1).IsEqualTo(winningHand);
+
+            var result2 = PokerHand.GetBestHand(losingHand, winningHand);
+            Check.That(result2).IsEqualTo(winningHand);
+        }
+    }
+}
